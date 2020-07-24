5d7c48fe65522fd0731558e6eb41a897cfb8a66f
Mady1947 <63644681+Mady1947@users.noreply.github.com>
2020-07-24 20:53:14 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,23 @@
-variable "region"{
-	default = "ap-south-1b"
+variable "aws_region" {
+  type    = string
+  default = "us-west-1"
 }
-variable "vpc_cidr"{
-	default = "190.160.0.0/16" 
+
+variable "db_table_name" {
+  type    = string
+  default = "terraform-learn"
 }
-variable "subnet_cidr"{
-	default = "190.160.1.0/24"
-	
+
+variable "db_read_capacity" {
+  type    = number
+  default = 1
+}
+
+variable "db_write_capacity" {
+  type    = number
+  default = 1
+}
+
+variable "tag_user_name" {
+  type = string
 }
