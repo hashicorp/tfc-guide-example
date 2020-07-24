8ca680c673500dfdd625e02013429aac318acdf8
Mady1947 <63644681+Mady1947@users.noreply.github.com>
2020-07-24 18:31:53 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,21 @@
-variable "aws_region" {
-  type    = string
-  default = "us-west-1"
+variable "region"{
+	default = "ap-south-1b"
 }
+variabl "vpc_cidr"{
+	default = "190.160.0.0/16" 
+}
+variabl "subnet_cidr"{
+	type = "list"
+	default = ["190.160.1.0/24","190.160.2.0/24","190.160..0/24"] 
+}
+#variabl "azs"{
+#	type= "list"
+#	default = ["ap-south-1a","ap-south-1b","ap-south-1c"] 
+#}
 
-variable "db_table_name" {
-  type    = string
-  default = "terraform-learn"
+# Declare the data source
+data "aws_availability_zones" "azs"{
+	
 }
 
 variable "db_read_capacity" {
