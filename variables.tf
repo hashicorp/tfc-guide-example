6d8edf5f3a1298035660913229312ec8a6472a20
Mady1947 <63644681+Mady1947@users.noreply.github.com>
2020-07-24 18:59:15 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,33 +1,10 @@
 variable "region"{
 	default = "ap-south-1b"
 }
-variable "vpc_cidr"{
+variabl "vpc_cidr"{
 	default = "190.160.0.0/16" 
 }
-variable "subnet_cidr"{
-	type = list(string)
-	default = ["190.160.1.0/24","190.160.2.0/24","190.160..0/24"] 
-}
-#variabl "azs"{
-#	type= list(string)
-#	default = ["ap-south-1a","ap-south-1b","ap-south-1c"] 
-#}
-
-# Declare the data source
-data "aws_availability_zones" "azs"{
+variabl "subnet_cidr"{
+	default = "190.160.1.0/24"
 	
-}
-
-variable "db_read_capacity" {
-  type    = number
-  default = 1
-}
-
-variable "db_write_capacity" {
-  type    = number
-  default = 1
-}
-
-variable "tag_user_name" {
-  type = string
 }
