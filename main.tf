61dd01b336e87781df6c10d202ca24962b5f101f
Mady1947 <63644681+Mady1947@users.noreply.github.com>
2020-07-24 19:18:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,22 @@
 provider "aws" {
-  version = "2.33.0"
-
-  region = var.aws_region
+  region = "ap-south-1b"
 }
-
-provider "random" {
-  version = "2.2"
-}
-
-resource "random_pet" "table_name" {}
-
-resource "aws_dynamodb_table" "tfc_example_table" {
-  name = "${var.db_table_name}-${random_pet.table_name.id}"
-
-  read_capacity  = var.db_read_capacity
-  write_capacity = var.db_write_capacity
-  hash_key       = "UUID"
-
-  attribute {
-    name = "UUID"
-    type = "S"
+resource "aws_vpc" "main" {
+  cidr_block = "$var.vpc_cidr"
+  instance_tenancy ="default"
+  
+  tags = {
+    Name = "main"
+    Location = "Mumbai"
   }
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id     = "$aws_vpc.main.id"
+  cidr_block = "$var.subnet_cidr"
 
   tags = {
-    user_name = var.tag_user_name
+    Name = "subnet1"
   }
 }
+
