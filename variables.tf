e9b7d0ac8057eb6c10fb6c96f28d56ca88f61306
Mady1947 <63644681+Mady1947@users.noreply.github.com>
2020-07-24 18:50:14 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "vpc_cidr"{
 	default = "190.160.0.0/16" 
 }
 variable "subnet_cidr"{
-	type = "list"
+	type = list(string)
 	default = ["190.160.1.0/24","190.160.2.0/24","190.160..0/24"] 
 }
 #variabl "azs"{
