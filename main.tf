5e0fb51c75c3c12cbb8eefdd943d8cf70e1e35e7
Mady1947 <63644681+Mady1947@users.noreply.github.com>
2020-07-27 20:13:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,7 @@
 provider "aws" {
-  region     = "ap-south-1"
-  access_key = "AKIATCINKSED47LL27GW"
-  secret_key = "9bQag+C2mq4ec0Yi9UU5iZ0Y5tXwPtnxEvT1y1U2"
-}
+  version = "2.33.0"
 
-
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+  region = var.aws_west
 }
 
 provider "random" {
@@ -22,9 +16,15 @@ resource "aws_dynamodb_table" "tfc_example_table" {
   read_capacity  = var.db_read_capacity
   write_capacity = var.db_write_capacity
   hash_key       = "UUID"
+  range_key      = "UserName"
 
   attribute {
     name = "UUID"
+    type = "S"
+  }
+
+  attribute {
+    name = "UserName"
     type = "S"
   }
 
