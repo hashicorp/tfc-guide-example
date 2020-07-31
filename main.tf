b48bf476446a9873889fcacd929ae8e28e08a734
roary tubbs <roary.tubbs@gmail.com>
2020-07-31 10:13:58 -0500

    Revert "Update main.tf"
    This reverts commit f04b2a738582ff3ad99df856f32dc68fdcaa8b82.

--- main.tf
+++ main.tf
@@ -1,11 +1,34 @@
-resource "random_id" "random" {
-  keepers = {
-    uuid = "${uuid()}"
-  }
+provider "aws" {
+  version = "2.33.0"
 
-  byte_length = 8
+  region = var.aws_west
 }
 
-output "random" {
-  value = "${random_id.random.hex}"
+provider "random" {
+  version = "2.2"
+}
+
+resource "random_pet" "table_name" {}
+
+resource "aws_dynamodb_table" "tfc_example_table" {
+  name = "${var.db_table_name}-${random_pet.table_name.id}"
+
+  read_capacity  = var.db_read_capacity
+  write_capacity = var.db_write_capacity
+  hash_key       = "UUID"
+  range_key      = "UserName"
+
+  attribute {
+    name = "UUID"
+    type = "S"
+  }
+
+  attribute {
+    name = "UserName"
+    type = "S"
+  }
+
+  tags = {
+    user_name = var.tag_user_name
+  }
 }
