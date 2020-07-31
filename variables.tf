5f769829decedc4295e6a514cbbb4b0173e8d629
Mady1947 <63644681+Mady1947@users.noreply.github.com>
2020-07-31 15:35:34 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,23 +1,3 @@
-variable "aws_region" {
-  type    = string
-  default = "ap-south-1"
-}
-
-variable "db_table_name" {
-  type    = string
-  default = "terraform-learn"
-}
-
-variable "db_read_capacity" {
-  type    = number
-  default = 1
-}
-
-variable "db_write_capacity" {
-  type    = number
-  default = 1
-}
-
-variable "tag_user_name" {
-  type = string
+variable "instance_type" {
+    type = string
 }
