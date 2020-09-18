b4f59f9e7a64f741d891f3d8ba195048486ae17b
Rob717171 <49822268+Rob717171@users.noreply.github.com>
2020-09-18 18:54:03 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,15 +10,9 @@ provider "random" {
 
 resource "random_pet" "table_name" {}
 
-resource "aws_dynamodb_table" "tfc_example_table" {
-  name = "${var.db_table_name}-${random_pet.table_name.id}"
+resource "aws_instance" "basic" {
+  ami           = "ami-0ee1a20d6b0c6a347"
+  instance_type = "t3.nano"
+}
 
-  read_capacity  = var.db_read_capacity
-  write_capacity = var.db_write_capacity
-  hash_key       = "UUID"
-
-  attribute {
-    name = "UUID"
-    type = "S"
-  }
 }
