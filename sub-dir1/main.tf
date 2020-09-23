f6dd6f26e575104250843463fbe1a86c6a821aeb
jeff-knurek <jeff.knurek@clevertech.biz>
2020-09-23 14:05:05 +0200

    adding new module

---
+++ sub-dir1/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  version = "2.33.0"
+
+  region = var.aws_region
+}
+
+provider "random" {
+  version = "2.2"
+}
+
+resource "random_pet" "table_name" {}
+
+resource "aws_dynamodb_table" "tfc_example_table" {
+  name = "${var.db_table_name}-${random_pet.table_name.id}"
+
+  read_capacity  = var.db_read_capacity
+  write_capacity = var.db_write_capacity
+  hash_key       = "UUID"
+
+  attribute {
+    name = "UUID"
+    type = "S"
+  }
+}
---
+++ sub-dir1/outputs.tf
@@ -0,0 +1,3 @@
+output "tfc_example_table_arn" {
+  value = aws_dynamodb_table.tfc_example_table.arn
+}
