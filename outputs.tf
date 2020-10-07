fb5fcb467714b62408f45dda002723bd055ccf3f
Clay Roach <clay@streamweaver.com>
2020-10-07 11:15:40 -0700

    trying to create cluster

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "eks_cluster_endpoint" {
+  value = aws_eks_cluster.aws_eks.endpoint
+}
+
+output "eks_cluster_certificat_authority" {
+  value = aws_eks_cluster.aws_eks.certificate_authority 
+}
--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
   type    = string
-  default = "us-west-1"
+  default = "us-east-1"
 }
 
 variable "db_table_name" {
