b8efd4cb5d80a4dddf114208301b6aa2a4710a5f
Clay Roach <clay@streamweaver.com>
2020-10-07 10:49:00 -0700

    Remove Dynamo DB

--- main.tf
+++ main.tf
@@ -7,18 +7,3 @@ provider "aws" {
 provider "random" {
   version = "2.2"
 }
-
-resource "random_pet" "table_name" {}
-
-resource "aws_dynamodb_table" "tfc_example_table" {
-  name = "${var.db_table_name}-${random_pet.table_name.id}"
-
-  read_capacity  = var.db_read_capacity
-  write_capacity = var.db_write_capacity
-  hash_key       = "UUID"
-
-  attribute {
-    name = "UUID"
-    type = "S"
-  }
-}
