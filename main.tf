6eb28c0fd3090870fd8d5a092ac41532c6a656e5
Seth Jensen <sjensen85@gmail.com>
2020-10-14 18:07:48 -0500

    fix references

--- main.tf
+++ main.tf
@@ -7,22 +7,22 @@ provider "azurerm" {
 # Create a resource group
 resource "azurerm_resource_group" "dev-ddp-rg" {
   name     = var.rg_name
-  location = "West Europe"
+  location = "East US 2"
 }
 
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "dev-ddp-vnet" {
   name                = var.vnet_name
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.dev-ddp-rg.name
+  location            = azurerm_resource_group.dev-ddp-rg.location
   address_space       = var.vnet_cidr_blocks
 }
 
 resource "azurerm_subnet" "dev-ddp-sub-dbr-1" {
   count                = 2
   name                 = var.subnet_names[count.index]
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.dev-ddp-rg.name
+  virtual_network_name = azurerm_virtual_network.dev-ddp-vnet.name
   address_prefixes     = var.subnet_cidr_blocks[count.index]
 
   service_endpoints = var.service_endpoints
