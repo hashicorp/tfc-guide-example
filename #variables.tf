afc670d301668362c10d83c1eaae3ff0f6b94ff5
Seth Jensen <sjensen85@gmail.com>
2020-10-14 18:08:52 -0500

    fix references

---
+++ #variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  type    = string
+  default = "us-west-1"
+}
+
+variable "db_table_name" {
+  type    = string
+  default = "terraform-learn"
+}
+
+variable "db_read_capacity" {
+  type    = number
+  default = 1
+}
+
+variable "db_write_capacity" {
+  type    = number
+  default = 1
+}
