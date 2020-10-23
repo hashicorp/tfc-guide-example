69fc61e9236aacb64e8de52001c5c9dcec390fa2
Seth Jensen <sjensen85@gmail.com>
2020-10-23 10:15:38 -0500

    add network watcher

--- modules/azure/resource_group/main.tf
+++ modules/azure/resource_group/main.tf
@@ -3,3 +3,9 @@ resource "azurerm_resource_group" "dev-ddp-rg" {
   name     = var.rg_name
   location = var.rg_region
 }
+
+resource "azurerm_network_watcher" "example" {
+  name                = "dbr-nwwatcher"
+  location            = azurerm_resource_group.dev-ddp-rg.location
+  resource_group_name = azurerm_resource_group.dev-ddp-rg.name
+}
