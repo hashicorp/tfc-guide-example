ee9e4348ad450c3d8e3bed2a989768dda2e101cf
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-02-03 20:46:46 +0900

    Update main.tf
    show me the money

--- main.tf
+++ main.tf
@@ -1,24 +1,20 @@
-provider "aws" {
-  version = "2.33.0"
-
-  region = var.aws_region
-}
-
-provider "random" {
-  version = "2.2"
-}
-
-resource "random_pet" "table_name" {}
-
-resource "aws_dynamodb_table" "tfc_example_table" {
-  name = "${var.db_table_name}-${random_pet.table_name.id}"
-
-  read_capacity  = var.db_read_capacity
-  write_capacity = var.db_write_capacity
-  hash_key       = "UUID"
-
-  attribute {
-    name = "UUID"
-    type = "S"
+terraform{
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
   }
+}
+
+provider "aws" {
+  profile = "default"
+  access_key="AKIA3F55JVDX76BDCJXS"
+  secret_key="mOojTaBCHmX64XA1WbkufI6ptCvLTsldRXGKUfD1"
+  region  = "ap-northeast-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c1ec05212231c930"
+  instance_type = "t2.micro"
 }
