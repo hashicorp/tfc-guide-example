644edec5169ecd6b314068fa4ec04da72c0621b8
Vinay Kumar.M <vinay.munivenkata@gmail.com>
2021-06-12 21:37:52 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,13 @@
 output "tfc_example_table_arn" {
   value = aws_dynamodb_table.tfc_example_table.arn
 }
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.app_server.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
+}
