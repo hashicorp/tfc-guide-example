f3de7bf91631eda77716ec3ee7a20d76162ce5d0
Umasankar Manda <umasankarmanda@Umasankars-MacBook-Pro.local>
2021-07-05 20:44:34 -0700

    chnaged root volume size

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "ubuntu" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
-  root_block_device = {
+  root_block_device {
   volume_size = "20"
   }
 
