497dd907ad4906512e5e505eecadef26af40ee50
Shane Holloman <shaneholloman@gmail.com>
2021-09-27 20:54:00 +1300

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "ubuntu" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
+  subnet_id     = "subnet-123abc12"
 
   tags = {
     Name = var.instance_name
