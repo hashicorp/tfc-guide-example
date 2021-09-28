068e0d74342a31856c5c20a4a24130384fae4af6
Jeremiah Callahan <jeremiahc@redapt.com>
2021-09-28 02:28:23 -0400

    Adding in VPC

--- main.tf
+++ main.tf
@@ -24,7 +24,30 @@ resource "aws_instance" "ubuntu" {
 
   tags = {
     Name = var.instance_name
-    "Linux Distribution" = "Ubuntu"
-    "Test_tag" = "Jeremiah"
+    linux distribution = "Ubuntu"
+    team = var.team
+    environment = var.environment
+    terraform = "true"
+  }
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.vpc-name
+  cidr = var.vpc-cidr
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    terraform = "true"
+    environment = var.environment
+    team = var.team
+    name = 
   }
 }
--- variables.tf
+++ variables.tf
@@ -13,3 +13,22 @@ variable "instance_name" {
   default     = "Provisioned by Terraform"
 }
 
+variable "vpc-name" {
+  description = "VPC Name"
+  default = "vpc-standard"
+}
+
+variable "environment" {
+  description = "Environment Variable"
+  default = "dev"
+}
+
+variable "team" {
+  description = "Mandatory tag to determine owning team"
+  default = "Not Modified"
+}
+
+variable "vpc-cidr" {
+  description = "VPC network cidr for all subnets"
+  default = "192.168.1.0/24"
+}
