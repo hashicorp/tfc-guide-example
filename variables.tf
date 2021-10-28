4e5952a479ea5136b4251eec25b18495dfde9cf0
Michail Pukhov <mik@inmircom.ru>
2021-10-28 15:43:59 +0300

    123

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "region" {
   description = "AWS region"
-  default     = "us-west-1"
+  default     = "us-east-2"
 }
 
 variable "instance_type" {
@@ -10,6 +10,6 @@ variable "instance_type" {
 
 variable "instance_name" {
   description = "EC2 instance name"
-  default     = "Provisioned by Terraform"
+  default     = "node"
 }
 
