f716baff81c84f821a4e1c4fc7a42fd3d559b5d7
Michail Pukhov <mik@inmircom.ru>
2021-11-01 15:28:04 +0300

    instans and VPS

--- main.tf
+++ main.tf
@@ -18,11 +18,62 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "ubuntu" {
+resource "aws_instance" "ubuntu1" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
   tags = {
-    Name = var.instance_name
+    Name = "node1"
+  }
+}
+
+resource "aws_instance" "ubuntu2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "node2"
+  }
+}
+
+resource "aws_instance" "ubuntu3" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "node3"
+  }
+}
+
+data "aws_region" "current" {}
+
+data "aws_instance" "ubuntu1" {
+  instance_id = aws_instance.ubuntu1.id
+}
+
+data "aws_instance" "ubuntu2" {
+  instance_id = aws_instance.ubuntu2.id
+}
+
+data "aws_instance" "ubuntu3" {
+  instance_id = aws_instance.ubuntu3.id
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "diplom-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = var.region
+  private_subnets = "10.0.1.0/24"
+  public_subnets  = "10.0.101.0/24"
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,23 @@ output "instance_arn" {
   value = aws_instance.ubuntu.arn
 }
 
+output "aws_region" {
+  value = data.aws_region.current.name
+}
+
+output "private_ip1" {
+  value = data.aws_instance.ubuntu1.private_ip
+}
+
+output "private_ip2" {
+  value = data.aws_instance.ubuntu2.private_ip
+}
+
+output "private_ip3" {
+  value = data.aws_instance.ubuntu3.private_ip
+}
+
+output "subnet_id" {
+  value = data.aws_instance.ubuntu1.subnet_id
+}
+
