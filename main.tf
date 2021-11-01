fce472800fe92c08ef9b965976d9f84ae91a4caa
Michail Pukhov <mik@inmircom.ru>
2021-11-01 15:55:21 +0300

    3

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ module "vpc" {
   name = "diplom-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = var.region
+  azs             = ["us-east-2"]
   private_subnets = ["10.0.1.0/24"]
   public_subnets  = ["10.0.101.0/24"]
 
