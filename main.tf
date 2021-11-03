3dbe5f8587836050caaa59ffffad3a0f884472e9
Michail Pukhov <mik@inmircom.ru>
2021-11-03 16:27:48 +0300

    7

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "ubuntu1" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
+  vpc_security_group_id = aws_security_group.Diplom.id
 
   tags = {
     Name = "node1"
@@ -30,6 +31,7 @@ resource "aws_instance" "ubuntu1" {
 resource "aws_instance" "ubuntu2" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
+  vpc_security_group_id = aws_security_group.Diplom.id
 
   tags = {
     Name = "node2"
@@ -39,6 +41,7 @@ resource "aws_instance" "ubuntu2" {
 resource "aws_instance" "ubuntu3" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
+  vpc_security_group_id = aws_security_group.Diplom.id
 
   tags = {
     Name = "node3"
@@ -65,9 +68,9 @@ module "vpc" {
   name = "diplom-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-2c"]
-  private_subnets = ["10.0.1.0/24"]
-  public_subnets  = ["10.0.101.0/24"]
+  azs             = ["us-east-2a","us-east-2b", "us-east-2c" ]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24" ]
+  public_subnets  = ["10.0.101.0/24","10.0.102.0/24", "10.0.103.0/24" ]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
@@ -75,5 +78,57 @@ module "vpc" {
   tags = {
     Terraform = "true"
     Environment = "dev"
+  }
+}
+
+resource "aws_security_group" "Diplom" {
+  name        = "Diplom"
+  description = "Allow TLS,SSH,HTTP inbound traffic"
+  vpc_id      = aws_vpc.diplom-vpc.id
+
+  ingress = [
+    {
+      description      = "TLS from VPC"
+      from_port        = 443
+      to_port          = 443
+      protocol         = "tcp"
+      cidr_blocks      = [aws_vpc.diplpm-vpc.cidr_block]
+      ipv6_cidr_blocks = [aws_vpc.diplom-vpc.ipv6_cidr_block]
+    } 
+    ,
+   
+    {
+      description      = "SSH from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = [aws_vpc.diplpm-vpc.cidr_block]
+      ipv6_cidr_blocks = [aws_vpc.diplom-vpc.ipv6_cidr_block]
+    }
+    ,
+
+    {
+      description      = "HTTP from VPC"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = [aws_vpc.diplpm-vpc.cidr_block]
+      ipv6_cidr_blocks = [aws_vpc.diplom-vpc.ipv6_cidr_block]
+    }
+
+  ] 
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  ]
+
+  tags = {
+    Name = "allov_ssh_http_tsl"
   }
 }
