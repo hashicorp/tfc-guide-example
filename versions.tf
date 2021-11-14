23c7f901a538f5fc7c4b16e0b8aac615fed4381a
Thomas <thom.laine@icloud.com>
2021-11-14 13:02:00 +0100

    add backend conf

--- versions.tf
+++ versions.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "daimoniac"
+
+    workspaces {
+      name = "test"
+    }
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
