86fbb93d2103c45390b1c11f75507da3c4b82b1a
toxinu <toxinu@gmail.com>
2022-02-01 15:06:38 +0000

    Add fakewebservices

--- main.tf
+++ main.tf
@@ -1,27 +1,26 @@
+provider "fakewebservices" {
+  token = var.provider_token
+}
 
-# provider "fakewebservices" {
-#   token = var.provider_token
-# }
+resource "fakewebservices_vpc" "primary_vpc" {
+  name = "Primary VPC"
+  cidr_block = "0.0.0.0/1"
+}
 
-# resource "fakewebservices_vpc" "primary_vpc" {
-#   name = "Primary VPC"
-#   cidr_block = "0.0.0.0/1"
-# }
+resource "fakewebservices_server" "servers" {
+  count = 2
 
-# resource "fakewebservices_server" "servers" {
-#   count = 2
+  name = "Server ${count.index+1}"
+  type = "t2.micro"
+  vpc = fakewebservices_vpc.primary_vpc.name
+}
 
-#   name = "Server ${count.index+1}"
-#   type = "t2.micro"
-#   vpc = fakewebservices_vpc.primary_vpc.name
-# }
+resource "fakewebservices_load_balancer" "primary_lb" {
+  name = "Primary Load Balancer"
+  servers = fakewebservices_server.servers[*].name
+}
 
-# resource "fakewebservices_load_balancer" "primary_lb" {
-#   name = "Primary Load Balancer"
-#   servers = fakewebservices_server.servers[*].name
-# }
-
-# resource "fakewebservices_database" "prod_db" {
-#   name = "Production DB"
-#   size = 256
-# }
+resource "fakewebservices_database" "prod_db" {
+  name = "Production DB"
+  size = 256
+}
