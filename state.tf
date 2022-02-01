1faa3e957d0f6b017205c9343af848de1bc5062c
toxinu <toxinu@gmail.com>
2022-02-01 15:04:27 +0000

    Remove state data

--- state.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "vpc" {
-  backend = "remote"
-  config = {
-    organization = "example-org-adedf3"
-    workspaces = {
-      name = "tfc-guide-example"
-    }
-  }
-}
