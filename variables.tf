d8745535dc67c13d1cb638f76a9a6c8f44f186fe
sandeept1986 <33664662+sandeept1986@users.noreply.github.com>
2022-03-13 19:29:44 +0530

    modified

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "region" {
 
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
-  default     = "t2.micro"
+  default     = "t2.nano"
 }
 
 variable "instance_name" {
