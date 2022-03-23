c5a124a56d4085b0ef561d645c29df8fbfe7b9f0
Long Nguyen D. [4] <81797874+mvn-longnguyen4-dn@users.noreply.github.com>
2022-03-23 19:30:45 +0700

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,12 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.0"
+      version = "~> 3.0"
     }
   }
 
