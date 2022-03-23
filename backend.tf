983ffedc3286ab00f561b6bf4ccad0b175cc77e6
Long Nguyen D. [4] <81797874+mvn-longnguyen4-dn@users.noreply.github.com>
2022-03-23 19:35:31 +0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,9 @@
-terraform {
-  backend "s3" {
-      bucket = "magellanic1"
-      key    = "terraform.tfstate"
-      region = "us-east-1"
-    }
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket = "magellanic1"
+    key    = "network/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
