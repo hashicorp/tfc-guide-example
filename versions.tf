113a335017a45a044945555f63b5ae956146a098
davidtnfsh <davidtnfsh@gmail.com>
2023-03-14 16:37:14 +0800

    test

--- versions.tf
+++ versions.tf
@@ -2,6 +2,13 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
+  cloud {
+    organization = "example-org-d63b9a"
+
+    workspaces {
+      name = "github_action"
+    }
+  }
 	required_providers {
 		google = {
 	    version = "~> 4.51.0"
